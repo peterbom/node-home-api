2c557f761d85167fe51de974db6db5084f8f9d0a
gituser <gituser@bombnas.com>
2016-08-01 15:36:29 +1200

    Deleted shrinkwrap file as it causes npm install to hang on prod server

--- src/oidc/oidc-client-settings.js
+++ src/oidc/oidc-client-settings.js
@@ -1,7 +1,7 @@
 // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-import {Log} from "../shared/Log";
+import {Log} from "../shared/log";
 
 const OidcMetadataUrlPath = ".well-known/openid-configuration";
 
