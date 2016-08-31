f5ec29305060b1c62afe7547fd1d3e20d572263c
gituser <gituser@bombnas.com>
2016-09-01 11:22:47 +1200

    Added timeout to exiftool

--- src/data-access/exif-tool.js
+++ src/data-access/exif-tool.js
@@ -2,6 +2,7 @@ import path from "path";
 import moment from "moment";
 
 var exec = require("child-process-promise").exec;
+const timeout = 60000; // 1 minute
 
 export class ExifTool {
     constructor () {
@@ -15,7 +16,7 @@ export class ExifTool {
     async getAllTags(filePath) {
         let command = `perl ${this._toolPath} -s -a -j "${filePath}"`;
 
-        let output = await exec(command);
+        let output = await exec(command, {timeout: timeout});
         if (output.stdout) {
             return JSON.parse(output.stdout)[0];
         } else {
@@ -53,7 +54,7 @@ export class ExifTool {
 
         let infos = [];
         try {
-            let output = await exec(command);
+            let output = await exec(command, {timeout: timeout});
 
             if (!output.stdout) {
                 if (output.stderr) {
