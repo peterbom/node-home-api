6362f43661116df88f107be41f58a4bb276d0d64
peter <pete_bomber@hotmail.com>
2017-09-15 13:34:05 +1200

    give testing token a longer lifetime

--- tasks/create-token.js
+++ tasks/create-token.js
@@ -2,11 +2,6 @@ const {Log} = require("../src/shared/log");
 const {EnvVarsProvider} = require("../src/shared/env-vars-provider");
 const {JwtUtils} = require("../src/shared/jwt-utils");
 
-// const {AzureSecretRetriever} = require("../src/shared/azure-secret-retriever");
-// const {AppSettingsBuilder} = require("../src/shared/app-settings-builder");
-// const {AppLauncher} = require("../src/app-launcher");
-// const config = require("../src/config");
-
 let envVarsProvider = new EnvVarsProvider();
 let envVars = envVarsProvider.getEnvVars();
 
@@ -18,7 +13,7 @@ let user = {
     aud: "ZZZZZZZZZZZ" // TODO: Fix when we are properly validating claims
 };
 
-let lifetimeInMinutes = 60 * 12; // valid for 12 hours
+let lifetimeInMinutes = 60 * 24 * 30; // valid for 30 days
 let token = jwtUtils.signJwt(user, lifetimeInMinutes);
 
 console.log(token);
